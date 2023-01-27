ce6db686affa15f80369385d137333f8aad1a735
Spandana <sservirala@ecisolutions.com>
2023-01-27 16:21:04 +0530

    changed data in deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.11.0'
 
-set :rvm_ruby_version, '2.4.2@default_project'
-set :application, 'default_project'
-set :repo_url, 'git@github.com:eci-rhc/lv-default-project.git'
+set :rvm_ruby_version, '2.4.2@testing-repo'
+set :application, 'testing-repo'
+set :repo_url, 'git@github.com:eci-rhc/lv-testing-repo.git'
 set :deploy_via, :remote_cache
 set :passenger_restart_with_touch, true
 
